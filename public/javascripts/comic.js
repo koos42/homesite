1e9d6cf6f9948c1fef339e119d1c374bd15f9ce6
koos42 <koos42@krikkit1.local>
2011-03-26 15:59:18 -0700

    whoops, forgot to add the comic.js

---
+++ public/javascripts/comic.js
@@ -0,0 +1,23 @@
+//this should be responsible for having the text blurb thing show up on hover over the comic.
+
+if( window.comic === undefined){
+  comic = {};
+}
+
+comic.setup = function(){
+  var blurb = $('#comic #blurb').hide()
+  blurb.css('position', 'absolute').css('margin', '30px 0 0 100px');
+  $('#photo img').add(blurb).hover(function(){
+                          blurb.show();
+                        }, 
+                        function(){
+                          blurb.hide();
+                        });
+};
+
+
+$(document).ready(function(){
+  comic.setup();
+});
+
+
