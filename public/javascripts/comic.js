dc0043948bbb25bf7c9fff973e620fe5ac1eac90
Koos Kleven <koos42@gmail.com>
2011-04-10 12:05:31 -0700

    added js to make showing the original comic more noticable.

--- public/javascripts/comic.js
+++ public/javascripts/comic.js
@@ -5,14 +5,36 @@ if( window.comic === undefined){
 }
 
 comic.setup = function(){
-  var blurb = $('#comic #blurb').hide()
+
+  comic.show_original = function(){
+    var original = $('.original_link');
+    comic.current_url = $('#photo img').attr('src');
+    $('#photo img').attr('src', original.attr('href'));
+  };
+
+  comic.unshow_original = function(){
+    $('#photo img').attr('src', comic.current_url);
+  };
+
+  $('#original').
+    hover(comic.show_original, comic.unshow_original).
+    click(function(){return false;});
+
+  var blurb = $('#comic #blurb').hide();
   blurb.css('position', 'absolute').css('margin', '30px 0 0 80px');
-  $('#photo img').add(blurb).hover(function(){
-                          blurb.show();
-                        }, 
-                        function(){
-                          blurb.hide();
-                        });
+  $('#photo img').
+    add(blurb).
+    hover(
+      function(){
+        blurb.show();
+      }, 
+      function(){
+        blurb.hide();
+      }).
+    click(
+      function(){
+        blurb.hide();
+      });
 };
 
 
