9a4b1cf2e2a556b783bd290f5a32ff14f04ee779
Koos Kleven <koos42@gmail.com>
2011-07-15 00:32:38 -0700

    made test pass a bit more.  TODO: figure out devise in tests

--- test/functional/pages_controller_test.rb
+++ test/functional/pages_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
   test "should get about" do
     get :about
     assert_response :success
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,10 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+include Devise::TestHelpers
+
 class ActiveSupport::TestCase
+
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
   #
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
