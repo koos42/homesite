6e944e4c6b8a5fdd4d780cc142f031cd1446a594
koos kleven <koos42@gmail.com>
2011-12-10 17:51:52 -0800

    added the appropriate status codes for the errors

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,10 +1,11 @@
 class ErrorsController < ApplicationController
 
   def four_oh_four
-    
+    render 'four_oh_four', :status => 404    
   end
 
   def five_hundred
+    render 'five_hundred', :status => 500
   end
 
 end
