d2a6a9133a7e82fe4ce5d73225d159f37532018a
koos kleven <koos42@gmail.com>
2012-07-26 15:06:31 -0700

    added a route and way for admins to delete goodies

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,28 @@
 Webcomic::Application.routes.draw do
-  resources :goodies
 
   root :to => "comics#latest"
 
   get "pages/about"
 
+# Users
   match 'users/sign_up' => 'comics#latest'
   post 'users' => 'users#create'
   devise_for :users
   resources :users
 
+# Comics
   match 'comics/feed' => 'comics#feed'
   match 'comics/latest' => 'comics#latest'
   match 'feed' => 'comics#feed',
         :as => 'feed',
         :defaults => { :format => 'atom' }
   match 'comics/:id/destroy' => 'comics#destroy'
-  
+
   resources :comics
+
+# Goodies
+  match 'goodies/:id/destroy' => 'goodies#destroy', :as => 'destroy_goody'
+  resources :goodies
 
   get "errors/four_oh_four"
   get "errors/five_hundred"
