0736636ec9da770cfcb62f7ba3f00667e29881c9
koos kleven <koos42@gmail.com>
2012-10-25 18:34:58 -0700

    stubs in some tests for the comics controller
    this doesn't add test, but just adds some places for them to go.
    I've still got to figure out what I'm going to do RE stubbing models,
    setting up a test db, or using FactoryGirl. Maybe all three for
    esses & gees?

---
+++ spec/controllers/comics_controller_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe ComicsController do
+  it 'shows a comic' do
+
+  end
+
+  it 'shows all the comics' do
+
+  end
+
+  it 'makes you sign to new a comic' do
+
+  end
+
+  it 'makes you sign in to create a comic' do
+
+  end
+
+  it 'makes you sign in to edit a comic' do
+
+  end
+
+  it 'makes you sign in to update a comic' do
+
+  end
+
+  it 'makes you sign in to destroy a comic' do
+
+  end
+
+  it 'redirects to slug routes when hit by an id route' do
+
+  end
+end
